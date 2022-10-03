769eba6e96a1f2f6c5dc782f30b95e49ec706f17
AndresCastil <casaned@yahoo.com>
2022-10-03 02:01:07 +0200

    Primer commit

---
+++ UD04_JavaBasics2/src/UD04Ejer3.java
@@ -0,0 +1,36 @@
+
+public class UD04Ejer3 {
+	public static void main(String[] args) {
+		
+		int X=8;
+		int Y=6;
+		double N=7.3;
+		double M=2.9;
+		
+		System.out.println("Variable X = "+X);
+		System.out.println("Variable Y = "+Y);
+		System.out.println("Variable N = "+N);
+		System.out.println("Variable M = " +M);
+		
+		System.out.println(X+ " + " +Y+" = "+ (X+Y));
+		System.out.println(X+ " - " +Y+ " = "+ (X-Y));
+		System.out.println(X+ " * " +Y+ " = "+ (X*Y));
+		System.out.println(X+ " / " +Y+ " = "+ (X/Y));
+		System.out.println(X+ " % " +Y+ " = "+ (X%Y));
+		System.out.println(N+ " + " +M+ " = "+ (N+M));
+		System.out.println(N+ " - " +M+ " = "+ (N-M));
+		System.out.println(N+ " * " +M+ " = "+ (N*M));
+		System.out.println(N+ " / " +M+ " = "+ (N/M));
+		System.out.println(N+ " % " +M+ " = "+ (N%M));
+		System.out.println(X+ " + " +N+" = "+ (X+N));
+		System.out.println(Y+ " / " +M+ " = "+ (Y/M));
+		System.out.println(Y+ " % " +M+ " = "+ (Y%M));
+		System.out.println("El doble de la variable X es: " +(X*2));
+		System.out.println("El doble de la variable Y es: " +(Y*2));
+		System.out.println("El doble de la variable N es: " +(N*2));
+		System.out.println("El doble de la variable M es: " +(M*2));
+		System.out.println("La suma de todas las variables es: " +(X+Y+N+M));
+		System.out.println("El producto de todas las variables es: " +(X*Y*N*M));
+		
+	}
+}
